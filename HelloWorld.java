a24c9aefda6759ad6a233c184347aaf2b5690a85
raremaza <103074019+raremaza@users.noreply.github.com>
2022-08-30 06:08:37 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        String message = new String();
+        message = "Mazurenko Nikita KI-36";
+        System.out.println(message);
+    }
+}
